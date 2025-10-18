913181769349f973cc84f0c7b3c1fc7f226b472f
jeffinprakash <jeffinprakash7@gmail.com>
2025-10-18 18:11:03 +0530

    Update hello_world.py
    hello_world.py — first Python program for AI/ML journey

--- 01_basics/hello_world.py
+++ 01_basics/hello_world.py
@@ -1,1 +1,9 @@
-print("Hello World")
+# hello_world.py
+# My first Python program in my AI/ML journey
+
+def main():
+    print("Hello, world!")
+    print("Starting my honest Python practice for AI/ML learning journey.")
+
+if __name__ == "__main__":
+    main()
